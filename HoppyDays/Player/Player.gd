00242f1f93484447b1ec3e035cc69f88e8e9ddd7
Alexey Lebedeff <binarin@binarin.ru>
2020-07-11 19:35:56 +0200

    More hoppy days

--- HoppyDays/Player/Player.gd
+++ HoppyDays/Player/Player.gd
@@ -1,13 +1,41 @@
 extends KinematicBody2D
 
-const SPEED = 500
+const SPEED = 1500
+const GRAVITY = 300
+const UP = Vector2(0, -1)
+const JUMP_SPEED = 5000
+
 var motion = Vector2(0, 0)
 
+signal animate
+
 func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP)
+	
+
+func animate():
+	emit_signal("animate", motion)
+
+	
+	
+func move():
 	motion.x = 0
 	if Input.is_action_pressed("left"):
 		motion.x -= SPEED
 	if Input.is_action_pressed("right"):
 		motion.x += SPEED
 		
-	move_and_slide(motion)
+	
+func apply_gravity():
+	if not is_on_floor():
+		motion.y += GRAVITY
+	else:
+		motion.y = 0
+		
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
